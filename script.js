f101e6235e71e74c665797bde8959b053367c04d
JovanGraovac <jovangraovac98@gmail.com>
2024-11-21 11:21:05 +0100

    Added logic for painting squares on mouse hover

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const NUMBER_OF_SQUARES = 16;
 const container = document.querySelector('#container');
 const grid = document.querySelector('#grid');
 
+// Function for generating grid
 function generateGrid(num) {
      
       for (let i = 0; i < num; i++) {
@@ -14,12 +15,20 @@ function generateGrid(num) {
                   const row = document.createElement('div');
                   row.classList.add('squares');
                   col.appendChild(row);
-            }
 
+                  // Paint squares on hover
+                  row.addEventListener('mouseover', () => {
+                  row.style.backgroundColor = getRandomColor()
+                  })
+            }
             container.appendChild(col);
       };  
 };
 
-
+// Function for generating random color
+function getRandomColor () {
+      return`#${Math.floor(Math.random() * 16777215).toString(16).padStart(6,'0')}`
+}
 
 generateGrid(NUMBER_OF_SQUARES);
+
